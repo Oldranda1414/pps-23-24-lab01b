2499a4f4a5633ae69a5b3593178ca98feac6638a
Oldranda <leonardorandacio@gmail.com>
2024-03-05 17:09:47 +0100

    red, green and refactor for correctPosition

--- src/main/java/e1/Knight.java
+++ src/main/java/e1/Knight.java
@@ -2,10 +2,15 @@ package e1;
 
 public class Knight implements Piece {
 
+    private Pair<Integer, Integer> position;
+
+    public Knight(Pair<Integer, Integer> position){
+       this.position = position;
+    }
+
     @Override
     public Pair<Integer, Integer> getPosition() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+        return this.position;
     }
 
     @Override
--- src/test/java/e1/KnightTest.java
+++ src/test/java/e1/KnightTest.java
@@ -1,5 +1,24 @@
 package e1;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class KnightTest {
+
+    private Piece knight;
+    private final Pair<Integer, Integer> initialPosition = new Pair<Integer,Integer>(0,0);
     
+    @BeforeEach
+    void beforeEach(){
+       knight = new Knight(initialPosition); 
+    }
+
+    @Test
+    void correctPosition(){
+        assertEquals(initialPosition, knight.getPosition());
+    }
 }
