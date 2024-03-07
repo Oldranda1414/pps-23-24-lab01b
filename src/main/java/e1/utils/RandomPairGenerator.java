6a3033df94a8fe94e1fac9c7ba1e4ddf0d2a1aab
Oldranda <leonardorandacio@gmail.com>
2024-03-07 17:49:53 +0100

    setup for third iteration of ex2 step 2

---
+++ src/main/java/e1/utils/RandomPairGenerator.java
@@ -0,0 +1,14 @@
+package e1.utils;
+
+import java.util.Random;
+
+public class RandomPairGenerator {
+
+    private Random random;
+
+    public Pair<Integer, Integer> nextPair() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'next'");
+    }
+    
+}
