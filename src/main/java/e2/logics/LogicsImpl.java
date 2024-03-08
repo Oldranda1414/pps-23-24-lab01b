68248f2cae9ec616a91c31ff9b2957cf2a9aed55
Oldranda <leonardorandacio@gmail.com>
2024-03-08 19:52:02 +0100

    implemented Grid into logics

--- src/main/java/e2/logics/LogicsImpl.java
+++ src/main/java/e2/logics/LogicsImpl.java
@@ -4,41 +4,35 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import e2.utils.Pair;
+import e2.grid.Cell;
+import e2.grid.CellType;
+import e2.grid.Grid;
+import e2.grid.GridImpl;
 
+//TODO CHECK IF NON RANDOM CONSTRUCTOR IS USED
+//TODO CHECK IF SIZE AND N_MINES ATTRIBUITES ARE NECESSARY
 public class LogicsImpl implements Logics {
 
     private int SIZE;
     private int N_MINES;
-    private ArrayList<Pair<Integer, Integer>> mines;
+    private Grid grid;
 
     public LogicsImpl(final int SIZE, final int N_MINES) {
         this.SIZE = SIZE;
         this.N_MINES = N_MINES;
-        this.mines = new ArrayList<Pair<Integer, Integer>>();
-        generateRandomMines();
+        this.grid = new GridImpl(this.SIZE, this.N_MINES);
     }
 
-    public LogicsImpl(final int SIZE, final int N_MINES, ArrayList<Pair<Integer, Integer>> mines) {
+    public LogicsImpl(final int SIZE, final int N_MINES, Grid grid) {
         this.SIZE = SIZE;
         this.N_MINES = N_MINES;
-        this.mines = mines;
+        this.grid = grid;
     }
 
     @Override
     public boolean hasMine(Pair<Integer, Integer> position) {
-        return this.mines.contains(position);
-    }
-
-    private void generateRandomMines(){
-        for(int i = 0; i < this.N_MINES; i++){
-            this.mines.add(generateRandomMine());
-        }
-    }
-
-    private Pair<Integer, Integer> generateRandomMine(){
-        final Random random = new Random();
-        final var pair = new Pair<>(random.nextInt(SIZE), random.nextInt(SIZE));
-        return this.mines.contains(pair) ? generateRandomMine() : pair;
+        Cell cell = this.grid.getCell(position.getX(), position.getY());
+        return cell.getType().equals(CellType.MINE);
     }
 
 }
