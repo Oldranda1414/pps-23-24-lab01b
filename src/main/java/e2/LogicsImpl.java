42b03d96d4c02117435e1f2aff1c619017e0144a
Oldranda <leonardorandacio@gmail.com>
2024-03-08 11:08:12 +0100

    red, green and refactor for testMineGeneration

--- src/main/java/e2/LogicsImpl.java
+++ src/main/java/e2/LogicsImpl.java
@@ -1,14 +1,42 @@
 package e2;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class LogicsImpl implements Logics {
 
-    public LogicsImpl(int size) {
+    private int SIZE;
+    private int N_MINES;
+    private ArrayList<Pair<Integer, Integer>> mines;
+
+    public LogicsImpl(final int SIZE, final int N_MINES) {
+        this.SIZE = SIZE;
+        this.N_MINES = N_MINES;
+        this.mines = new ArrayList<Pair<Integer, Integer>>();
+        generateRandomMines();
+    }
+
+    public LogicsImpl(final int SIZE, final int N_MINES, ArrayList<Pair<Integer, Integer>> mines) {
+        this.SIZE = SIZE;
+        this.N_MINES = N_MINES;
+        this.mines = mines;
     }
 
     @Override
     public boolean hasMine(Pair<Integer, Integer> position) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'hasMine'");
+        return this.mines.contains(position);
+    }
+
+    private void generateRandomMines(){
+        for(int i = 0; i < this.N_MINES; i++){
+            this.mines.add(generateRandomMine());
+        }
+    }
+
+    private Pair<Integer, Integer> generateRandomMine(){
+        final Random random = new Random();
+        final var pair = new Pair<>(random.nextInt(SIZE), random.nextInt(SIZE));
+        return this.mines.contains(pair) ? generateRandomMine() : pair;
     }
 
 }
--- src/test/java/e2/LogicsTest.java
+++ src/test/java/e2/LogicsTest.java
@@ -1,14 +1,29 @@
 package e2;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class LogicsTest {
     
     private Logics logics;
     private final int SIZE = 9;
+    private final int N_MINES = 4;
 
     @BeforeEach
     void beforeEach(){
-        logics = new LogicsImpl(SIZE);
+        logics = new LogicsImpl(SIZE, N_MINES);
+    }
+
+    @Test
+    void testMineGeneration(){
+        int minesCounter = 0;
+        for(int x = 0; x < SIZE; x++){
+            for(int y = 0; y < SIZE; y++){
+                if(logics.hasMine(new Pair<>(x,y)))minesCounter++;
+            }
+        }
+       assertEquals(N_MINES, minesCounter); 
     }
 }
