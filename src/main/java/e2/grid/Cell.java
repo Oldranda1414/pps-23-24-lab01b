8ed08cf6ca4495dc9ff3e2efbdb6960ace32f8e2
Oldranda <leonardorandacio@gmail.com>
2024-03-08 11:32:49 +0100

    setup for CellTest

---
+++ src/main/java/e2/grid/Cell.java
@@ -0,0 +1,8 @@
+package e2.grid;
+
+public interface Cell {
+
+    public int adiacentMines();
+    
+    public boolean isMine();
+}
---
+++ src/main/java/e2/grid/CellImpl.java
@@ -0,0 +1,17 @@
+package e2.grid;
+
+public class CellImpl implements Cell{
+
+    @Override
+    public int adiacentMines() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'adiacentMines'");
+    }
+
+    @Override
+    public boolean isMine() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isMine'");
+    }
+    
+}
