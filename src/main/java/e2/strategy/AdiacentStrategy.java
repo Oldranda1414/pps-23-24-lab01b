635c4335f92098a1cee41695316b5dd8f54b40af
Oldranda <leonardorandacio@gmail.com>
2024-03-09 12:42:22 +0100

    created NumberStratgy and implementation and tests

---
+++ src/main/java/e2/strategy/AdiacentStrategy.java
@@ -0,0 +1,39 @@
+package e2.strategy;
+
+import e2.grid.Cell;
+import e2.grid.CellType;
+
+public class AdiacentStrategy implements NumberStrategy{
+
+
+    @Override
+    public int calculateNumber(Cell[][] cells, int X, int Y) {
+        return countAdiacentMines(cells, X, Y, cells.length, cells[0].length);
+    }
+
+    private int countAdiacentMines(Cell[][] cells, int X, int Y, int SIZE_X, int SIZE_Y){
+        int counter = 0;
+
+        final int[] ADIACENT_X = {X, X - 1, X + 1};
+        final int[] ADIACENT_Y = {Y, Y - 1, Y + 1};
+
+        for(var x : ADIACENT_X){
+            for(var y : ADIACENT_Y){
+                if(x != X && y != Y && isValidPosition(x, y, SIZE_X, SIZE_Y)){
+                    Cell currentCell = cells[x][y];
+                    if(currentCell.getType().equals(CellType.MINE)) counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    private boolean isValidPosition(final int X, final int Y, int SIZE_X, int SIZE_Y){
+
+        if(X < SIZE_X && X >= 0 && Y < SIZE_Y && Y >= 0){
+            return true;
+        }
+        return false;
+    }
+    
+}
